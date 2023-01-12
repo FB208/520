1f54e60b589703ba08d51125d52f5e1df40fb2fc
fb208 <472189065@qq.com>
2023-01-12 13:44:25 +0800

    2023年新内容

--- js/talk.js
+++ js/talk.js
@@ -8,13 +8,70 @@ new TypeIt("#tqyher", {
     .delete(null, {
         delay: 500
     })
-    .type("一起走过的第十个520...")
+    .type("一起走过了11个年头")
+    .pause(3000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("我们都已到而立之年")
+    .pause(3000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("二人世界变成了三口之家")
+    .pause(3000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("~未来会更好~")
     .pause(3000)
     .go();
 
-new TypeIt('#talkList', {
-    lifeLike: true,
-    cursorSpeed: 1000,
-    waitUntilVisible: true,
-    speed: 100
-}).go();
+
+
+    new TypeIt("#talkList", {
+        // loop: true,
+        cursorSpeed: 1000,
+        speed: 80
+    })
+    .type("十年<br/><br/>")
+    .type("不知不觉已经走过十年，从激情四射到习以为常<br/><br/>")
+    .type("回眼望去，这十年越来越被生活的琐事所束缚<br/><br/>")
+    .type("为了脚下的六便士，忘却了抬眼望月的美好<br/><br/>")
+    .type("未来的十年，会是一个新的开始<br/><br/>")
+    .type("从今天开始改变自己<br/><br/>")
+    .type("我想<br/>")
+    .type("从今天开始，做些更有意义的事情，过些更有意思的生活，做一个激情四射的自己<br/><br/>")
+    .type("这个站点，永久保留，将来把它改成一个相册回忆录<br/><br/>")
+    .type("记录我们的十年、二十年、三十年、一百年<br/><br/>")
+    .type("看完了嘛<br/>")
+    .pause(1000)
+    .type("看完了不？<br/>")
+    .pause(2000)
+    .type("翻页了啊！<br/>")
+    .pause(3000)
+    .delete(null, {
+        delay: 1000
+    })
+    .type("我们都已到而立之年<br/><br/>")
+    .type("二人世界变成了三口之家<br/><br/>")
+    .type("我会学着如何做好一个父亲<br/><br/>")
+    .type("也会更努力地去做好一个丈夫<br/><br/>")
+    .type("感谢你十几年来的支持与包容<br/><br/>")
+    .type("2023年，我们携手共进<br/><br/>")
+    .pause(3000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("~未来会更好~<br/><br/>")
+    .pause(2000)
+    .type("❤")
+    .pause(500)
+    .type("爱")
+    .pause(1000)
+    .type("你")
+    .pause(1000)
+    .type("哦")
+    .pause(1000)
+    .type("❤")
+    .go();
